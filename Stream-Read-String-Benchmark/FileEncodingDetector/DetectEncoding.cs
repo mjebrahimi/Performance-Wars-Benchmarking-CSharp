0f47e841f052dac70a1957a21e90614e9990768f
Mohammad Javad Ebrahimi <mj.ebrahimi@alibaba.ir>
2023-12-05 20:49:37 +0330

    Text Encoding Detection added

---
+++ Stream-Read-String-Benchmark/FileEncodingDetector/DetectEncoding.cs
@@ -0,0 +1,156 @@
+﻿using System.Text;
+
+namespace FileEncodingDetector;
+
+public static class DetectEncoding
+{
+    public static void Detect()
+    {
+        foreach (var str in new string[] { "a", "س" })
+        {
+            var utf8 = WriterTest.GetBytes_UTF8(str);
+            var utf8BOM = WriterTest.GetBytes_UTF8_BOM(str);
+            var utf16LE = WriterTest.GetBytes_UTF16_LE(str);
+            var utf16BE = WriterTest.GetBytes_UTF16_BE(str);
+            var utf16LEBOM = WriterTest.GetBytes_UTF16_LE_BOM(str);
+            var utf16BEBOM = WriterTest.GetBytes_UTF16_BE_BOM(str);
+
+            {
+                //var aaa1 = TestReader.GetString(utf8);
+                //var aaa2 = TestReader.GetString(utf8BOM);
+                //var aaa3 = TestReader.GetString(utf16LE);
+                //var aaa4 = TestReader.GetString(utf16BE);
+                //var aaa5 = TestReader.GetString(utf16LEBOM);
+                //var aaa6 = TestReader.GetString(utf16BEBOM);
+            }
+            {
+                var aaa1 = TextFileEncodingDetector.DetectTextByteArrayEncoding(utf8);
+                var aaa2 = TextFileEncodingDetector.DetectTextByteArrayEncoding(utf8BOM);
+                var aaa3 = TextFileEncodingDetector.DetectTextByteArrayEncoding(utf16LE);
+                var aaa4 = TextFileEncodingDetector.DetectTextByteArrayEncoding(utf16BE);
+                var aaa5 = TextFileEncodingDetector.DetectTextByteArrayEncoding(utf16LEBOM);
+                var aaa6 = TextFileEncodingDetector.DetectTextByteArrayEncoding(utf16BEBOM);
+            }
+            {
+                var aaa1 = EncodingUtilities.DetectEncoding(new MemoryStream(utf8));
+                var aaa2 = EncodingUtilities.DetectEncoding(new MemoryStream(utf8BOM));
+                var aaa3 = EncodingUtilities.DetectEncoding(new MemoryStream(utf16LE));
+                var aaa4 = EncodingUtilities.DetectEncoding(new MemoryStream(utf16BE));
+                var aaa5 = EncodingUtilities.DetectEncoding(new MemoryStream(utf16LEBOM));
+                var aaa6 = EncodingUtilities.DetectEncoding(new MemoryStream(utf16BEBOM));
+            }
+        }
+    }
+}
+
+public static class TestReader
+{
+    public static string GetString(byte[] bytes)
+    {
+        using var memoryStream = new MemoryStream(bytes);
+        using var streamReader = new StreamReader(memoryStream, detectEncodingFromByteOrderMarks: true);
+        return streamReader.ReadToEnd();
+    }
+}
+
+public static class WriterTest
+{
+    public static byte[] GetBytes_UTF8(string str)
+    {
+        var utf8 = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: false); //Without BOM encoding
+        using var memoryStream = new MemoryStream();
+        using var writer = new StreamWriter(memoryStream, utf8);
+        writer.Write(str);
+        writer.Flush();
+        return memoryStream.ToArray();
+    }
+
+    public static byte[] GetBytes_UTF8_BOM(string str)
+    {
+        var utf8_BOM = new UTF8Encoding(encoderShouldEmitUTF8Identifier: true, throwOnInvalidBytes: false); //With BOM encoding
+        using var memoryStream = new MemoryStream();
+        using var writer = new StreamWriter(memoryStream, utf8_BOM);
+        writer.Write(str);
+        writer.Flush();
+        return memoryStream.ToArray();
+    }
+
+    public static byte[] GetBytes_UTF16_LE(string str)
+    {
+        var utf16_LE = new UnicodeEncoding(bigEndian: false, byteOrderMark: false); //With BOM encoding
+        using var memoryStream = new MemoryStream();
+        using var writer = new StreamWriter(memoryStream, utf16_LE);
+        writer.Write(str);
+        writer.Flush();
+        return memoryStream.ToArray();
+    }
+
+    public static byte[] GetBytes_UTF16_BE(string str)
+    {
+        var utf16_BE = new UnicodeEncoding(bigEndian: true, byteOrderMark: false); //With BOM encoding
+        using var memoryStream = new MemoryStream();
+        using var writer = new StreamWriter(memoryStream, utf16_BE);
+        writer.Write(str);
+        writer.Flush();
+        return memoryStream.ToArray();
+    }
+
+    public static byte[] GetBytes_UTF16_LE_BOM(string str)
+    {
+        var utf16_LE_BOM = new UnicodeEncoding(bigEndian: false, byteOrderMark: true); //With BOM encoding
+        using var memoryStream = new MemoryStream();
+        using var writer = new StreamWriter(memoryStream, utf16_LE_BOM);
+        writer.Write(str);
+        writer.Flush();
+        return memoryStream.ToArray();
+    }
+
+    public static byte[] GetBytes_UTF16_BE_BOM(string str)
+    {
+        var utf16_BE_BOM = new UnicodeEncoding(bigEndian: true, byteOrderMark: true); //With BOM encoding
+        using var memoryStream = new MemoryStream();
+        using var writer = new StreamWriter(memoryStream, utf16_BE_BOM);
+        writer.Write(str);
+        writer.Flush();
+        return memoryStream.ToArray();
+    }
+}
+
+public static class EncoderTest
+{
+    public static byte[] GetBytes_UTF8(string str)
+    {
+        var utf8 = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: false); //Without BOM encoding
+        return utf8.GetBytes(str);
+    }
+
+    public static byte[] GetBytes_UTF8_BOM(string str)
+    {
+        var utf8_BOM = new UTF8Encoding(encoderShouldEmitUTF8Identifier: true, throwOnInvalidBytes: false); //With BOM encoding
+        return utf8_BOM.GetBytes(str);
+    }
+
+    public static byte[] GetBytes_UTF16_LE(string str)
+    {
+        var utf16_LE = new UnicodeEncoding(bigEndian: false, byteOrderMark: true); //With BOM encoding
+        return utf16_LE.GetBytes(str);
+    }
+
+    public static byte[] GetBytes_UTF16_BE(string str)
+    {
+        var utf16_BE = new UnicodeEncoding(bigEndian: true, byteOrderMark: true); //With BOM encoding
+        return utf16_BE.GetBytes(str);
+    }
+
+    public static byte[] GetBytes_UTF16_LE_BOM(string str)
+    {
+        var utf16_LE_BOM = new UnicodeEncoding(bigEndian: false, byteOrderMark: true); //With BOM encoding
+        return utf16_LE_BOM.GetBytes(str);
+    }
+
+    public static byte[] GetBytes_UTF16_BE_BOM(string str)
+    {
+        var utf16_BE_BOM = new UnicodeEncoding(bigEndian: true, byteOrderMark: true); //With BOM encoding
+        return utf16_BE_BOM.GetBytes(str);
+    }
+}
---
+++ Stream-Read-String-Benchmark/FileEncodingDetector/EncodingUtilities.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+namespace FileEncodingDetector;
+
+public static class EncodingUtilities
+{
+    public static Encoding DetectEncoding(string path)
+    {
+        using var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        return DetectEncoding(stream);
+    }
+
+    /// <summary>
+    /// UTF8    : EF BB BF
+    /// UTF16 BE: FE FF
+    /// UTF16 LE: FF FE
+    /// UTF32 BE: 00 00 FE FF
+    /// UTF32 LE: FF FE 00 00
+    /// </summary>
+    public static Encoding DetectEncoding(Stream stream)
+    {
+        if (!stream.CanSeek || !stream.CanRead)
+            throw new Exception("DetectEncoding() requires a seekable and readable Stream");
+
+        // Try to read 4 bytes. If the stream is shorter, less bytes will be read.
+        var u8_Buf = new byte[4];
+        int s32_Count = stream.Read(u8_Buf, 0, 4);
+        if (s32_Count >= 2)
+        {
+            if (u8_Buf[0] == 0xFE && u8_Buf[1] == 0xFF)
+            {
+                stream.Position = 2;
+                return new UnicodeEncoding(true, true);
+            }
+
+            if (u8_Buf[0] == 0xFF && u8_Buf[1] == 0xFE)
+            {
+                if (s32_Count >= 4 && u8_Buf[2] == 0 && u8_Buf[3] == 0)
+                {
+                    stream.Position = 4;
+                    return new UTF32Encoding(false, true);
+                }
+                else
+                {
+                    stream.Position = 2;
+                    return new UnicodeEncoding(false, true);
+                }
+            }
+
+            if (s32_Count >= 3 && u8_Buf[0] == 0xEF && u8_Buf[1] == 0xBB && u8_Buf[2] == 0xBF)
+            {
+                stream.Position = 3;
+                return Encoding.UTF8;
+            }
+
+            if (s32_Count >= 4 && u8_Buf[0] == 0 && u8_Buf[1] == 0 && u8_Buf[2] == 0xFE && u8_Buf[3] == 0xFF)
+            {
+                stream.Position = 4;
+                return new UTF32Encoding(true, true);
+            }
+        }
+
+        stream.Position = 0;
+        return Encoding.Default;
+    }
+}
