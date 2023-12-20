a78e8fab3c5434c187bcbbc1024065f309fc8091
Mohammad Javad Ebrahimi <mj.ebrahimi@alibaba.ir>
2023-12-20 10:09:06 +0330

    String StartsWith using Span benchmark added

---
+++ String-StartWith-Benchmark/Benchmark.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+
+#if RELEASE
+//[DryJob] //Don't use for real benchmark (Just for Test)
+//[ShortRunJob]
+[SimpleJob(BenchmarkDotNet.Engines.RunStrategy.Throughput)]
+#endif
+[KeepBenchmarkFiles(false)]
+[MemoryDiagnoser(displayGenColumns: false)]
+[Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+[GroupBenchmarksBy(BenchmarkDotNet.Configs.BenchmarkLogicalGroupRule.ByParams)]
+public class Benchmark
+{
+    [Arguments("http://www.google.com")]
+    [Arguments("https://www.google.com")]
+    [Benchmark]
+    public bool String_StartsWith(string str)
+    {
+        return str.StartsWith("https://");
+    }
+
+    [Arguments("http://www.google.com")]
+    [Arguments("https://www.google.com")]
+    [Benchmark]
+    public bool Span_StartsWith(string str)
+    {
+        return str.AsSpan().StartsWith("https://".AsSpan());
+    }
+
+    [Arguments("http://www.google.com")]
+    [Arguments("https://www.google.com")]
+    [Benchmark]
+    public bool SpanExt_StartsWith(string str)
+    {
+        return str.StartsWithSpan("https://");
+    }
+
+    #region Other Methods
+    //Regex.IsMatch(str, "^Hello");
+    //String.Compare(str, "Hello", StringComparison.Ordinal) == 0
+    #endregion
+}
+
+public static class StringExtensions
+{
+    public static bool StartsWithSpan(this string str, ReadOnlySpan<char> value)
+    {
+        return str.AsSpan().StartsWith(value);
+    }
+}
---
+++ String-StartWith-Benchmark/README.md
@@ -0,0 +1,7 @@
+# Different ways to check StartsWith for strings
+
+## Key Result
+
+1. `Span_StartsWith` is **so much faster** than `String_StartsWith`
+
+![Benchmark](Benchmark.png)
