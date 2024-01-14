c129dfaeacbe8a46d3032a9581ed31b56d4385ef
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-01-14 23:51:16 +0330

    Update dependencies

--- Enumerable-NullOrEmpty-Benchmark/Program.cs
+++ Enumerable-NullOrEmpty-Benchmark/Program.cs
@@ -1,21 +1,5 @@
-using BenchmarkDotNet.Running;
+using BenchmarkDotNetVisualizer;
 
-#if DEBUG
-
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("***** To achieve accurate results, set project configuration to Release mode. *****");
-
-Console.ForegroundColor = ConsoleColor.Red;
-Console.WriteLine("***** Waite 3 seconds for DEBUG MODE! *****");
-
-Thread.Sleep(3000);
-
-BenchmarkRunner.Run<Benchmark2>(new DebugInProcessConfigDry());
-
-#else
-
-BenchmarkRunner.Run<Benchmark2>();
-
-#endif
+BenchmarkAutoRunner.Run<Benchmark2>();
 
 Console.ReadLine();
--- String-StartWith-Benchmark/Program.cs
+++ String-StartWith-Benchmark/Program.cs
@@ -1,23 +1,5 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNetVisualizer;
 
-#if DEBUG
-
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("***** To achieve accurate results, set project configuration to Release mode. *****");
-
-Console.ForegroundColor = ConsoleColor.Red;
-Console.WriteLine("***** Waite 3 seconds for DEBUG MODE! *****");
-
-Thread.Sleep(3000);
-
-//BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfigDry()); //For Debugging
-BenchmarkRunner.Run<Benchmark>(new DebugInProcessConfigDry());
-
-#else
-
-//BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args); //InProcessEmitToolchain or InProcessNoEmitToolchain
-BenchmarkRunner.Run<Benchmark>();
-
-#endif
+BenchmarkAutoRunner.Run<Benchmark>();
 
 Console.ReadLine();
