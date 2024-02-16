3477f6c5a5dcdd38c6896f07ec4f5167d4005dd5
e.faridi <erfanffa2020@gmail.com>
2024-02-16 13:27:33 +0330

    feat: add png & html benchmark reports using benchmarkVisualizer

--- Convert-Enum-To-String-Benchmark/Program.cs
+++ Convert-Enum-To-String-Benchmark/Program.cs
@@ -1,15 +1,27 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNetVisualizer;
 using Convert_Enum_To_String_Benchmark;
 
 class Program
 {
-    public static void Main(string[] args)
+    public static async  Task Main(string[] args)
     {
 #if DEBUG
         Console.ForegroundColor = System.ConsoleColor.Yellow;
         Console.WriteLine("*****To achieve accurate results, set project configuration to Release mode.*****");
         return;
 #endif
-        BenchmarkRunner.Run<Benchmark>();
+        var summary = BenchmarkAutoRunner.Run<Benchmark>();
+
+        await summary.SaveAsHtmlAndImageAsync(
+            htmlPath: DirectoryHelper.GetPathRelativeToProjectDirectory("Reports\\VisualizedBenchmark.html"),
+            imagePath: DirectoryHelper.GetPathRelativeToProjectDirectory("Reports\\VisualizedBenchmark.png"),
+            options: new ReportHtmlOptions
+            {
+                Title = "Converting Enum To A String Benchmarks",
+                GroupByColumns = ["Method"],
+                SpectrumColumns = ["Mean", "Allocated"],
+                DividerMode = RenderTableDividerMode.EmptyDividerRow,
+                HtmlWrapMode = HtmlDocumentWrapMode.Simple
+            });
     }
 }
