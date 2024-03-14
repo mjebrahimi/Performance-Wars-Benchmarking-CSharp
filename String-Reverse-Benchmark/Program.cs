a9aef19acd03e7e5db38623978c0596383998b73
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-03-14 23:46:09 +0330

    Update String-Reverse-Benchmark

--- String-Reverse-Benchmark/Program.cs
+++ String-Reverse-Benchmark/Program.cs
@@ -1,27 +1,47 @@
 ﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Engines;
 using BenchmarkDotNet.Order;
-using BenchmarkDotNet.Running;
+using BenchmarkDotNetVisualizer;
 using System.Text;
 
-#if DEBUG
-System.Console.ForegroundColor = System.ConsoleColor.Yellow;
-System.Console.WriteLine("*****To achieve accurate results, set project configuration to Release mode.*****");
-return;
-#endif
-BenchmarkRunner.Run<Benchmark>();
+var summary = BenchmarkAutoRunner.Run<Benchmark>();
+
+await summary.SaveAsImageAsync(
+    path: DirectoryHelper.GetPathRelativeToProjectDirectory("Benchmark.png"),
+    options: new ReportHtmlOptions
+    {
+        Title = "Benchmark of different methods to Reverse a string",
+        SpectrumColumns = ["Mean", "Allocated"],
+        SortByColumns = ["Mean", "Allocated"],
+        HighlightGroups = false,
+    });
 
 Console.ReadLine();
 
-//[DryJob]
-//[ShortRunJob]
-[SimpleJob(RunStrategy.Throughput)]
+#if RELEASE
+[SimpleJob(BenchmarkDotNet.Engines.RunStrategy.Throughput)]
+#endif
 [MemoryDiagnoser]
-[KeepBenchmarkFiles(false)]
-[Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Declared)]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
 public class Benchmark
 {
     const string text = "Although most people consider piranhas to be quite dangerous, they are, for the most part, entirely harmless. Piranhas rarely feed on large animals.they eat smaller fish and aquatic plants.";
+
+    [Benchmark]
+    public string Span_Reverse_ZeroAllocation()
+    {
+        Span<char> span = text.GetSpan();
+        span.Reverse();
+        return text;
+    }
+
+    [Benchmark]
+    public string Span_Reverse()
+    {
+        Span<char> span = text.ToCharArray(); //OR (no difference)
+        //Span<char> span = text.AsSpan().ToArray();
+        span.Reverse();
+        return new string(span); //span.ToString(); //the same
+    }
 
     [Benchmark]
     public string Array_Reverse()
@@ -29,14 +49,6 @@ public class Benchmark
         var charArray = text.ToCharArray();
         Array.Reverse(charArray);
         return new string(charArray);
-    }
-
-    [Benchmark]
-    public string Span_Reverse()
-    {
-        Span<char> span = text.ToCharArray();
-        span.Reverse();
-        return new string(span); //span.ToString(); //the same
     }
 
     [Benchmark]
@@ -80,7 +92,7 @@ public class Benchmark
     [Benchmark]
     public string StringWriter()
     {
-        var writer = new StringWriter(); // uses underlying stringbuilder
+        var writer = new StringWriter(); // uses StringBuilder under the hood
         for (int i = text.Length - 1; i >= 0; i--)
             writer.Write(text[i]);
         return writer.ToString();
@@ -96,5 +108,20 @@ public class Benchmark
     public string LinqReverse_StringJoin()
     {
         return string.Join("", text.Reverse().ToArray());
+    }
+}
+
+public static class StringExtensions
+{
+    public static unsafe Span<char> GetSpan(this string str)
+    {
+        //var handle = GCHandle.Alloc(str, GCHandleType.Pinned);
+        //var address = handle.AddrOfPinnedObject();
+        //return new Span<char>(address.ToPointer(), str.Length);
+
+        fixed (char* ptr = str)
+        {
+            return new Span<char>(ptr, str.Length);
+        }
     }
 }
