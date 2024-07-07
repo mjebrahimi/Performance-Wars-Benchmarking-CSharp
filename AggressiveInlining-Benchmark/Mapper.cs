0b968487755130914e3abf42d3de73f5509755b0
Mohammad Javad Ebrahimi <mj.ebrahimi72@gmail.com>
2024-07-07 23:09:42 +0330

    Complete AggressiveInlining-Benchmark

--- AggressiveInlining-Benchmark/Mapper.cs
+++ AggressiveInlining-Benchmark/Mapper.cs
@@ -4,392 +4,98 @@ using System.Runtime.CompilerServices;
 public static class Mapper
 {
     #region Class
-    public static MyClass MapSimplified_NoAggressiveInlining(MyClassDto source)
-    {
-        return new MyClass
-        {
-            AlbumType = source.AlbumType,
-            Href = source.Href,
-            Id = source.Id,
-            Name = source.Name,
-            Popularity = source.Popularity,
-            ReleaseDate = source.ReleaseDate,
-            ReleaseDatePrecision = source.ReleaseDatePrecision,
-            Track = new MySubClass
-            {
-                Href = source.Track.Href,
-                Limit = source.Track.Limit,
-                Offset = source.Track.Offset,
-                Total = source.Track.Total
-            },
-            Type = source.Type,
-            Uri = source.Uri
-        };
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static MyClass MapSimplified_AggressiveInlining(MyClassDto source)
-    {
-        return new MyClass
-        {
-            AlbumType = source.AlbumType,
-            Href = source.Href,
-            Id = source.Id,
-            Name = source.Name,
-            Popularity = source.Popularity,
-            ReleaseDate = source.ReleaseDate,
-            ReleaseDatePrecision = source.ReleaseDatePrecision,
-            Track = new MySubClass
-            {
-                Href = source.Track.Href,
-                Limit = source.Track.Limit,
-                Offset = source.Track.Offset,
-                Total = source.Track.Total
-            },
-            Type = source.Type,
-            Uri = source.Uri
-        };
-    }
-
-    public static MyClass Map_NoAggressiveInlining(MyClassDto source)
+    public static MyClass Map(MyClassDto source)
     {
         var target = new MyClass();
-        target.AlbumType = source.AlbumType;
-        target.Href = source.Href;
-        target.Id = source.Id;
-        target.Name = source.Name;
-        target.Popularity = source.Popularity;
-        target.ReleaseDate = source.ReleaseDate;
-        target.ReleaseDatePrecision = source.ReleaseDatePrecision;
-        target.Track = MapToClassTracks_NoAggressiveInlining(source.Track);
-        target.Type = source.Type;
-        target.Uri = source.Uri;
+        target.Int = source.Int;
+        target.String = source.String;
+        target.Boolean = source.Boolean;
+        target.Long = source.Long;
+        target.Double = source.Double;
+        target.DateTime = source.DateTime;
+        target.Enum = source.Enum;
+        target.SubClass = MapSubClass(source.SubClass);
         return target;
     }
 
-    private static MySubClass MapToClassTracks_NoAggressiveInlining(MySubClassDto source)
+    private static MySubClass MapSubClass(MySubClassDto source)
     {
         var target = new MySubClass();
-        target.Href = source.Href;
-        target.Limit = source.Limit;
-        target.Offset = source.Offset;
-        target.Total = source.Total;
+        target.Int = source.Int;
+        target.String = source.String;
         return target;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static MyClass Map_AggressiveInlining(MyClassDto source)
+    public static MyClass MapAggressiveInlining(MyClassDto source)
     {
         var target = new MyClass();
-        target.AlbumType = source.AlbumType;
-        target.Href = source.Href;
-        target.Id = source.Id;
-        target.Name = source.Name;
-        target.Popularity = source.Popularity;
-        target.ReleaseDate = source.ReleaseDate;
-        target.ReleaseDatePrecision = source.ReleaseDatePrecision;
-        target.Track = MapToClassTracks_AggressiveInlining(source.Track);
-        target.Type = source.Type;
-        target.Uri = source.Uri;
+        target.Int = source.Int;
+        target.String = source.String;
+        target.Boolean = source.Boolean;
+        target.Long = source.Long;
+        target.Double = source.Double;
+        target.DateTime = source.DateTime;
+        target.Enum = source.Enum;
+        target.SubClass = MapSubClassAggressiveInlining(source.SubClass);
         return target;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static MySubClass MapToClassTracks_AggressiveInlining(MySubClassDto source)
+    private static MySubClass MapSubClassAggressiveInlining(MySubClassDto source)
     {
         var target = new MySubClass();
-        target.Href = source.Href;
-        target.Limit = source.Limit;
-        target.Offset = source.Offset;
-        target.Total = source.Total;
+        target.Int = source.Int;
+        target.String = source.String;
         return target;
     }
     #endregion
 
     #region Struct
-    public static MyStruct MapSimplified_NoAggressiveInlining(MyStructDto source)
-    {
-        return new MyStruct
-        {
-            AlbumType = source.AlbumType,
-            Href = source.Href,
-            Id = source.Id,
-            Name = source.Name,
-            Popularity = source.Popularity,
-            ReleaseDate = source.ReleaseDate,
-            ReleaseDatePrecision = source.ReleaseDatePrecision,
-            Track = new MySubStruct
-            {
-                Href = source.Track.Href,
-                Limit = source.Track.Limit,
-                Offset = source.Track.Offset,
-                Total = source.Track.Total
-            },
-            Type = source.Type,
-            Uri = source.Uri
-        };
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static MyStruct MapSimplified_AggressiveInlining(MyStructDto source)
-    {
-        return new MyStruct
-        {
-            AlbumType = source.AlbumType,
-            Href = source.Href,
-            Id = source.Id,
-            Name = source.Name,
-            Popularity = source.Popularity,
-            ReleaseDate = source.ReleaseDate,
-            ReleaseDatePrecision = source.ReleaseDatePrecision,
-            Track = new MySubStruct
-            {
-                Href = source.Track.Href,
-                Limit = source.Track.Limit,
-                Offset = source.Track.Offset,
-                Total = source.Track.Total
-            },
-            Type = source.Type,
-            Uri = source.Uri
-        };
-    }
-
-    public static MyStruct Map_NoAggressiveInlining(MyStructDto source)
+    public static MyStruct Map(MyStructDto source)
     {
         var target = new MyStruct();
-        target.AlbumType = source.AlbumType;
-        target.Href = source.Href;
-        target.Id = source.Id;
-        target.Name = source.Name;
-        target.Popularity = source.Popularity;
-        target.ReleaseDate = source.ReleaseDate;
-        target.ReleaseDatePrecision = source.ReleaseDatePrecision;
-        target.Track = MapToStructTracks_NoAggressiveInlining(source.Track);
-        target.Type = source.Type;
-        target.Uri = source.Uri;
+        target.Int = source.Int;
+        target.String = source.String;
+        target.Boolean = source.Boolean;
+        target.Long = source.Long;
+        target.Double = source.Double;
+        target.DateTime = source.DateTime;
+        target.Enum = source.Enum;
+        target.SubStruct = MapSubStruct(source.SubStruct);
         return target;
     }
 
-    private static MySubStruct MapToStructTracks_NoAggressiveInlining(MySubStructDto source)
+    private static MySubStruct MapSubStruct(MySubStructDto source)
     {
         var target = new MySubStruct();
-        target.Href = source.Href;
-        target.Limit = source.Limit;
-        target.Offset = source.Offset;
-        target.Total = source.Total;
+        target.Int = source.Int;
+        target.String = source.String;
         return target;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static MyStruct Map_AggressiveInlining(MyStructDto source)
+    public static MyStruct MapAggressiveInlining(MyStructDto source)
     {
         var target = new MyStruct();
-        target.AlbumType = source.AlbumType;
-        target.Href = source.Href;
-        target.Id = source.Id;
-        target.Name = source.Name;
-        target.Popularity = source.Popularity;
-        target.ReleaseDate = source.ReleaseDate;
-        target.ReleaseDatePrecision = source.ReleaseDatePrecision;
-        target.Track = MapToStructTracks_AggressiveInlining(source.Track);
-        target.Type = source.Type;
-        target.Uri = source.Uri;
+        target.Int = source.Int;
+        target.String = source.String;
+        target.Boolean = source.Boolean;
+        target.Long = source.Long;
+        target.Double = source.Double;
+        target.DateTime = source.DateTime;
+        target.Enum = source.Enum;
+        target.SubStruct = MapSubStructAggressiveInlining(source.SubStruct);
         return target;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static MySubStruct MapToStructTracks_AggressiveInlining(MySubStructDto source)
+    private static MySubStruct MapSubStructAggressiveInlining(MySubStructDto source)
     {
         var target = new MySubStruct();
-        target.Href = source.Href;
-        target.Limit = source.Limit;
-        target.Offset = source.Offset;
-        target.Total = source.Total;
+        target.Int = source.Int;
+        target.String = source.String;
         return target;
     }
-    #endregion
-
-    #region Comment
-    //#region Class
-    //public static MyClass MapSimplified(MyClassDto source)
-    //{
-    //    return new MyClass()
-    //    {
-    //        Int = source.Int,
-    //        Boolean = source.Boolean,
-    //        DateTime = source.DateTime,
-    //        Decimal = source.Decimal,
-    //        Double = source.Double,
-    //        Enum = source.Enum,
-    //        Long = source.Long,
-    //        String = source.String,
-    //        SubClass = new MySubClass
-    //        {
-    //            Int = source.SubClass.Int,
-    //            String = source.SubClass.String
-    //        }
-    //    };
-    //}
-
-    //[MethodImpl(MethodImplOptions.AggressiveInlining)]
-    //public static MyClass MapSimplifiedAggressiveInlining(MyClassDto source)
-    //{
-    //    return new MyClass()
-    //    {
-    //        Int = source.Int,
-    //        Boolean = source.Boolean,
-    //        DateTime = source.DateTime,
-    //        Decimal = source.Decimal,
-    //        Double = source.Double,
-    //        Enum = source.Enum,
-    //        Long = source.Long,
-    //        String = source.String,
-    //        SubClass = new MySubClass
-    //        {
-    //            Int = source.SubClass.Int,
-    //            String = source.SubClass.String
-    //        }
-    //    };
-    //}
-
-    //public static MyClass MapNoAggressiveInlining(MyClassDto source)
-    //{
-    //    var target = new MyClass();
-    //    target.Int = source.Int;
-    //    target.Boolean = source.Boolean;
-    //    target.DateTime = source.DateTime;
-    //    target.Decimal = source.Decimal;
-    //    target.Double = source.Double;
-    //    target.Enum = source.Enum;
-    //    target.Long = source.Long;
-    //    target.String = source.String;
-    //    target.SubClass = MapSubClassNoAggressiveInlining(source.SubClass);
-    //    return target;
-    //}
-
-    //private static MySubClass MapSubClassNoAggressiveInlining(MySubClassDto source)
-    //{
-    //    var target = new MySubClass();
-    //    target.Int = source.Int;
-    //    target.String = source.String;
-    //    return target;
-    //}
-
-    //[MethodImpl(MethodImplOptions.AggressiveInlining)]
-    //public static MyClass MapAggressiveInlining(MyClassDto source)
-    //{
-    //    var target = new MyClass();
-    //    target.Int = source.Int;
-    //    target.Boolean = source.Boolean;
-    //    target.DateTime = source.DateTime;
-    //    target.Decimal = source.Decimal;
-    //    target.Double = source.Double;
-    //    target.Enum = source.Enum;
-    //    target.Long = source.Long;
-    //    target.String = source.String;
-    //    target.SubClass = MapSubClassAggressiveInlining(source.SubClass);
-    //    return target;
-    //}
-
-    //[MethodImpl(MethodImplOptions.AggressiveInlining)]
-    //private static MySubClass MapSubClassAggressiveInlining(MySubClassDto source)
-    //{
-    //    var target = new MySubClass();
-    //    target.Int = source.Int;
-    //    target.String = source.String;
-    //    return target;
-    //}
-    //#endregion
-
-    //#region Struct
-    //public static MyStruct MapSimplified(MyStructDto source)
-    //{
-    //    return new MyStruct()
-    //    {
-    //        Int = source.Int,
-    //        Boolean = source.Boolean,
-    //        DateTime = source.DateTime,
-    //        Decimal = source.Decimal,
-    //        Double = source.Double,
-    //        Enum = source.Enum,
-    //        Long = source.Long,
-    //        String = source.String,
-    //        SubStruct = new MySubStruct
-    //        {
-    //            Int = source.SubStruct.Int,
-    //            String = source.SubStruct.String
-    //        }
-    //    };
-    //}
-
-    //[MethodImpl(MethodImplOptions.AggressiveInlining)]
-    //public static MyStruct MapSimplifiedAggressiveInlining(MyStructDto source)
-    //{
-    //    return new MyStruct()
-    //    {
-    //        Int = source.Int,
-    //        Boolean = source.Boolean,
-    //        DateTime = source.DateTime,
-    //        Decimal = source.Decimal,
-    //        Double = source.Double,
-    //        Enum = source.Enum,
-    //        Long = source.Long,
-    //        String = source.String,
-    //        SubStruct = new MySubStruct
-    //        {
-    //            Int = source.SubStruct.Int,
-    //            String = source.SubStruct.String
-    //        }
-    //    };
-    //}
-
-    //public static MyStruct MapNoAggressiveInlining(MyStructDto source)
-    //{
-    //    var target = new MyStruct();
-    //    target.Int = source.Int;
-    //    target.Boolean = source.Boolean;
-    //    target.DateTime = source.DateTime;
-    //    target.Decimal = source.Decimal;
-    //    target.Double = source.Double;
-    //    target.Enum = source.Enum;
-    //    target.Long = source.Long;
-    //    target.String = source.String;
-    //    target.SubStruct = MapSubStructNoAggressiveInlining(source.SubStruct);
-    //    return target;
-    //}
-
-    //private static MySubStruct MapSubStructNoAggressiveInlining(MySubStructDto source)
-    //{
-    //    var target = new MySubStruct();
-    //    target.Int = source.Int;
-    //    target.String = source.String;
-    //    return target;
-    //}
-
-    //[MethodImpl(MethodImplOptions.AggressiveInlining)]
-    //public static MyStruct MapAggressiveInlining(MyStructDto source)
-    //{
-    //    var target = new MyStruct();
-    //    target.Int = source.Int;
-    //    target.Boolean = source.Boolean;
-    //    target.DateTime = source.DateTime;
-    //    target.Decimal = source.Decimal;
-    //    target.Double = source.Double;
-    //    target.Enum = source.Enum;
-    //    target.Long = source.Long;
-    //    target.String = source.String;
-    //    target.SubStruct = MapSubStructAggressiveInlining(source.SubStruct);
-    //    return target;
-    //}
-
-    //[MethodImpl(MethodImplOptions.AggressiveInlining)]
-    //private static MySubStruct MapSubStructAggressiveInlining(MySubStructDto source)
-    //{
-    //    var target = new MySubStruct();
-    //    target.Int = source.Int;
-    //    target.String = source.String;
-    //    return target;
-    //}
-    //#endregion
     #endregion
 }
--- AggressiveInlining-Benchmark/Program.cs
+++ AggressiveInlining-Benchmark/Program.cs
@@ -3,13 +3,23 @@ using BenchmarkDotNet.Order;
 using BenchmarkDotNetVisualizer;
 using Models;
 
-BenchmarkAutoRunner.Run<Benchmark>(args);
+var summary = BenchmarkAutoRunner.Run<Benchmark>();
+
+await summary.SaveAsImageAsync(
+    path: DirectoryHelper.GetPathRelativeToProjectDirectory("Benchmark.png"),
+    options: new ReportHtmlOptions
+    {
+        Title = "AggressiveInlining Benchmark",
+        GroupByColumns = ["Categories"],
+        SpectrumColumns = ["Mean", "Allocated"],
+        SortByColumns = ["Mean", "Allocated"],
+        HighlightGroups = true,
+    });
 
 Console.ReadLine();
 
 #if RELEASE
-//[SimpleJob(BenchmarkDotNet.Engines.RunStrategy.Throughput)]
-[ShortRunJob]
+[SimpleJob(BenchmarkDotNet.Engines.RunStrategy.Throughput)]
 #endif
 [CategoriesColumn]
 [MemoryDiagnoser(displayGenColumns: false)]
@@ -19,133 +29,49 @@ public class Benchmark
 {
     public static readonly MyStructDto myStructDto = new MyStructDto
     {
-        AlbumType = "AlbumType",
-        Href = "Href",
-        Id = "Id",
-        Name = "Name",
-        Popularity = 100,
-        ReleaseDate = "ReleaseDate",
-        ReleaseDatePrecision = "ReleaseDatePrecision",
-        Type = "Type",
-        Uri = "Uri",
-        Track = new MySubStructDto
+        Int = 100,
+        String = "String",
+        Boolean = true,
+        Long = 100,
+        Double = 100.0,
+        DateTime = DateTime.Now,
+        Enum = MyEnum.Enum1,
+        SubStruct = new MySubStructDto
         {
-            Href = "Href",
-            Limit = 100,
-            Offset = 100,
-            Total = 100
+            Int = 100,
+            String = "String"
         }
     };
 
     public static readonly MyClassDto myClassDto = new MyClassDto
     {
-        AlbumType = "AlbumType",
-        Href = "Href",
-        Id = "Id",
-        Name = "Name",
-        Popularity = 100,
-        ReleaseDate = "ReleaseDate",
-        ReleaseDatePrecision = "ReleaseDatePrecision",
-        Type = "Type",
-        Uri = "Uri",
-        Track = new MySubClassDto
+        Int = 100,
+        String = "String",
+        Boolean = true,
+        Long = 100,
+        Double = 100.0,
+        DateTime = DateTime.Now,
+        Enum = MyEnum.Enum1,
+        SubClass = new MySubClassDto
         {
-            Href = "Href",
-            Limit = 100,
-            Offset = 100,
-            Total = 100
+            Int = 100,
+            String = "String"
         }
     };
 
     #region Class
-    [Benchmark(Description = "MapSimplified_AggressiveInlining"), BenchmarkCategory("Class")]
-    public MyClass MapSimplified_AggressiveInlining_Class() => Mapper.MapSimplified_AggressiveInlining(myClassDto);
+    [Benchmark(Description = "MapAggressiveInlining"), BenchmarkCategory("Class")]
+    public MyClass MapAggressiveInlining_Class() => Mapper.MapAggressiveInlining(myClassDto);
 
-    [Benchmark(Description = "MapSimplified_NoAggressiveInlining"), BenchmarkCategory("Class")]
-    public MyClass MapSimplified_NoAggressiveInlining_Class() => Mapper.MapSimplified_NoAggressiveInlining(myClassDto);
-
-    [Benchmark(Description = "Map_AggressiveInlining"), BenchmarkCategory("Class")]
-    public MyClass Map_AggressiveInlining_Class() => Mapper.Map_AggressiveInlining(myClassDto);
-
-    [Benchmark(Description = "Map_NoAggressiveInlining"), BenchmarkCategory("Class")]
-    public MyClass Map_NoAggressiveInlining_Class() => Mapper.Map_NoAggressiveInlining(myClassDto);
+    [Benchmark(Description = "Map"), BenchmarkCategory("Class")]
+    public MyClass Map_Class() => Mapper.Map(myClassDto);
     #endregion
 
     #region Struct
-    [Benchmark(Description = "MapSimplified_AggressiveInlining"), BenchmarkCategory("Struct")]
-    public MyStruct MapSimplified_AggressiveInlining_Struct() => Mapper.MapSimplified_AggressiveInlining(myStructDto);
+    [Benchmark(Description = "MapAggressiveInlining"), BenchmarkCategory("Struct")]
+    public MyStruct MapAggressiveInlining_Struct() => Mapper.MapAggressiveInlining(myStructDto);
 
-    [Benchmark(Description = "MapSimplified_NoAggressiveInlining"), BenchmarkCategory("Struct")]
-    public MyStruct MapSimplified_NoAggressiveInlining_Struct() => Mapper.MapSimplified_NoAggressiveInlining(myStructDto);
-
-    [Benchmark(Description = "Map_AggressiveInlining"), BenchmarkCategory("Struct")]
-    public MyStruct Map_AggressiveInlining_Struct() => Mapper.Map_AggressiveInlining(myStructDto);
-
-    [Benchmark(Description = "Map_NoAggressiveInlining"), BenchmarkCategory("Struct")]
-    public MyStruct Map_NoAggressiveInlining_Struct() => Mapper.Map_NoAggressiveInlining(myStructDto);
-    #endregion
-
-    #region Comment
-    //public static readonly MyStructDto myStructDto = new MyStructDto
-    //{
-    //    Int = 100,
-    //    Boolean = true,
-    //    DateTime = DateTime.Now,
-    //    Decimal = 100.0m,
-    //    Double = 100.0,
-    //    Enum = MyEnum.Enum1,
-    //    Long = 100,
-    //    String = "String",
-    //    SubStruct = new MySubStructDto
-    //    {
-    //        Int = 100,
-    //        String = "String"
-    //    }
-    //};
-
-    //public static readonly MyClassDto myClassDto = new MyClassDto
-    //{
-    //    Int = 100,
-    //    Boolean = true,
-    //    DateTime = DateTime.Now,
-    //    Decimal = 100.0m,
-    //    Double = 100.0,
-    //    Enum = MyEnum.Enum1,
-    //    Long = 100,
-    //    String = "String",
-    //    SubClass = new MySubClassDto
-    //    {
-    //        Int = 100,
-    //        String = "String"
-    //    }
-    //};
-
-    //#region Class
-    //[Benchmark, BenchmarkCategory("Class")]
-    //public MyClass MapSimplifiedAggressiveInlining_Class() => Mapper.MapSimplifiedAggressiveInlining(myClassDto);
-
-    //[Benchmark, BenchmarkCategory("Class")]
-    //public MyClass MapAggressiveInlining_Class() => Mapper.MapAggressiveInlining(myClassDto);
-
-    //[Benchmark, BenchmarkCategory("Class")]
-    //public MyClass MapSimplified_Class() => Mapper.MapSimplified(myClassDto);
-
-    //[Benchmark, BenchmarkCategory("Class")]
-    //public MyClass MapNoAggressiveInlining_Class() => Mapper.MapNoAggressiveInlining(myClassDto);
-    //#endregion
-
-    //#region Struct
-    //[Benchmark, BenchmarkCategory("Struct")]
-    //public MyStruct MapSimplifiedAggressiveInlining_Struct() => Mapper.MapSimplifiedAggressiveInlining(myStructDto);
-
-    //[Benchmark, BenchmarkCategory("Struct")]
-    //public MyStruct MapAggressiveInlining_Struct() => Mapper.MapAggressiveInlining(myStructDto);
-
-    //[Benchmark, BenchmarkCategory("Struct")]
-    //public MyStruct MapSimplified_Struct() => Mapper.MapSimplified(myStructDto);
-
-    //[Benchmark, BenchmarkCategory("Struct")]
-    //public MyStruct MapNoAggressiveInlining_Struct() => Mapper.MapNoAggressiveInlining(myStructDto);
-    //#endregion
+    [Benchmark(Description = "Map"), BenchmarkCategory("Struct")]
+    public MyStruct Map_Struct() => Mapper.Map(myStructDto);
     #endregion
 }
